6d221ae9ea8b137c00c65123e7458f7a1279bb59
Hasla <richkibz@gmail.com>
2023-06-27 20:05:16 +0300

    creating a report back

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,9 @@
 export default function createReportObject(employeesList) {
-    return {
-        allEmployees:
-         {...employeesList, },
-        getNumberOfDepartments(emp) {
-          return Object.keys(emp).length;
-        }, 
-    };
+  return {
+    allEmployees:
+      { ...employeesList, },
+    getNumberOfDepartments(emp) {
+      return Object.keys(emp).length;
+    },
+  };
 }
