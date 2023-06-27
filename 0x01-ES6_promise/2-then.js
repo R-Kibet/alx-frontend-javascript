1db5eaa3fa7746bb078bdca8417d469040399b1a
Hasla <richkibz@gmail.com>
2023-06-28 02:53:55 +0300

    appending handlers to promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-      console.log('Got a response from the API');
-    } else {
-      reject(new Error(''));
-    }
+    const suc = { status: 200, body: 'Success' };
+    return promise
+      .then(() => suc)
+      .catch(() => new Error())
+      .finally(() => { console.log('Got a response from the API'); });
   });
 }
