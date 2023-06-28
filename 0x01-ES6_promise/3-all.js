8aef30b2107c5dd5711f9ecb438b74825c0b3a3b
Hasla <richkibz@gmail.com>
2023-06-28 22:22:28 +0300

    mltiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((data) => {
-    const body = data[0].body;
-    const first = data[1].firstName;
-    const lst = data[1].lastName;
-    console.log(`${body} ${first} ${lst}`);
+    console.log(`${data[0].body} ${data[1].firstName} ${ data[1].lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
   });
