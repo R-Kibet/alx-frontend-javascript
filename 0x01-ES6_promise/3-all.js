c3302c49e8d18ecf70cb0aaf07d726ca383f9d28
Hasla <richkibz@gmail.com>
2023-06-28 21:12:02 +0300

    mltiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  return new Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
       console.log(`${data[0].body}, ${data[1].firstName}, ${data[1].lastName}`);
     }).catch(() =>
