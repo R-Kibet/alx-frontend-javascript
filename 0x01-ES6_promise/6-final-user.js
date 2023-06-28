4137d9315277899c3702a2144c89d9edb4146370
Hasla <richkibz@gmail.com>
2023-06-28 23:17:28 +0300

    promiss .settleall

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,23 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((data) => {
+      const arr = [];
+      data.forEach((dt) => {
+        if (dt.status === 'fulfilled') {
+          arr.push({
+            status: dt.status,
+            value: dt.value,
+          });
+        } else {
+          arr.push({
+            status: dt.status,
+            value: `Error: ${dt.reason}`,
+          });
+        }
+      });
+      return arr;
+    });
+}
