3a2ca341d2bfacd2bb7ebc8c2f1ab28cc44b8f5e
Hasla <richkibz@gmail.com>
2023-06-29 13:54:21 +0300

    getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._len = length;
+    this._stud = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._len;
+  }
+
+  get studnt() {
+    return this._stud;
+  }
+
+  set name(x) {
+    if (typeof x !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = `${x}`;
+  }
+
+  set length(x) {
+    if (typeof x !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._len = `${x}`;
+  }
+
+  set studnt(x) {
+    this._stud = `${x}`;
+  }
+}
