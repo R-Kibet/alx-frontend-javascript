235efc3c3fb20a42466b7e7babcd69201a1eb88c
Hasla <richkibz@gmail.com>
2023-06-29 15:49:33 +0300

    abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
-  constructor(sqft = 0) {
-    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+  constructor(sqft) {
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
