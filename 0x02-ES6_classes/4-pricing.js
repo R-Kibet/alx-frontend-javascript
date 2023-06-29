e49385d201c71291d03d1b1f0b0219156df53d23
Hasla <richkibz@gmail.com>
2023-06-29 15:21:43 +0300

    static method

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line
 import Currency from './3-currency';
 
 export default class Pricing {
@@ -20,6 +19,9 @@ export default class Pricing {
   }
 
   set curr(x) {
+    if (!(x instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of class Currency');
+    }
     this._currency = x;
   }
 
