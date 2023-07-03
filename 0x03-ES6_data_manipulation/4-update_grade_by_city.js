d73372d488205f484f70872387b76fc5c0303e51
Hasla <richkibz@gmail.com>
2023-07-04 01:06:14 +0300

    combning filter and map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,5 +2,6 @@ export default function updateStudentGradeByCity(student, city, newGrades) {
   return student.filter((loc) => loc.location === city)
     .map((std) => ({
       ...std,
+      // eslint-disable-next-line
       grade: newGrades.filter((grd) => grd.studentId === std.id).map((g) => g.grade)[0] || 'N/A' }));
 }
