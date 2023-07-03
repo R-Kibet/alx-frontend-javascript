27183e321fac22d6c3e508df7e3feece2e0d770c
Hasla <richkibz@gmail.com>
2023-07-03 19:40:40 +0300

    objects an arrays

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -5,13 +5,11 @@ export default function getListStudents() {
       firstname: 'Guillaume',
       location: 'San Francisco'
     },
-
     {
       id: 2,
       firstname: 'James',
       location: 'Columbia'
     },
-
     {
       id: 5,
       firstname: 'Serena',
